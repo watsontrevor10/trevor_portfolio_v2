57f9fdda335d412239feb10e65166f25e39fdeb9
Trevor Watson <watsontrevor10@gmail.com>
2020-03-05 23:09:04 -0700

    got main 4 placeholder images set

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,29 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import styled from 'styled-components'
 import Layout from "../components/layout"
-import Image from "../components/image"
+import MusicImage from "../components/musicImage"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <ImageWrapper>
+        <MusicImage />
+        <MusicImage />
+        <MusicImage />
+        <MusicImage />
+    </ImageWrapper>
   </Layout>
 )
+
+const ImageWrapper = styled.div`
+  display: Flex;
+  flex-direction: row;
+  justify-content: space-between;
+  .gatsby-image-wrapper {
+    width: 300px;
+  }
+`
 
 export default IndexPage
