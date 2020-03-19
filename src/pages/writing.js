c36bf6c696cd79c479dffbdc1f416f7a3e03fa7e
watsontrevor10 <watsontrevor10@gmail.com>
2020-03-19 16:02:40 -0600

    got Wordpress api pulling blog posts

--- src/pages/writing.js
+++ src/pages/writing.js
@@ -1,13 +1,50 @@
-import React from 'react'
-import Layout from '../components/layout'
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+// import Blog from "../components/blog"
 
-const Blog = () => {
-
-    return (
-        <Layout>
-            <h1>Blog</h1>
-        </Layout>
-    )
+const Writing = ({ data }) => {
+  return (
+    <Layout>
+      <h1>Blog</h1>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "center",
+        }}
+      >
+        {/* <Blog /> */}
+        {data.allWordpressPost.edges.map(post => (
+          <div style={{ maxWidth: "600px", padding: `0 1.0875rem 1.45rem` }}>
+            <h1 dangerouslySetInnerHTML={{ __html: post.node.title }} />
+            <p>{post.node.date}</p>
+            <div dangerouslySetInnerHTML={{ __html: post.node.excerpt }} />
+          </div>
+        ))}
+      </div>
+    </Layout>
+  )
 }
 
-export default Blog
+export const query = graphql`
+  query {
+    allWordpressPost {
+      edges {
+        node {
+          id
+          author
+          slug
+          title
+          date(formatString: "MMMM DD, YYYY")
+          excerpt
+          content
+          modified
+        }
+      }
+    }
+  }
+`
+
+export default Writing
