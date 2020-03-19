13ad95ea90dc19ec4b1922feeefd84360c5ff7dd
watsontrevor10 <watsontrevor10@gmail.com>
2020-03-19 15:14:49 -0600

    added wordpress plugin back in and configured

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,45 @@ module.exports = {
         username: `chivsjawnphoto`,
       },
     },
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        // I have created a dummy site for us to use with the plugins we discussed
+        baseUrl: "chivsjawn.wordpress.com/",
+        protocol: "https",
+        hostingWPCOM: true,
+        // We will be using some advanced custom fields
+        useACF: false,
+        acfOptionPageIds: [],
+        verboseOutput: false,
+        perPage: 100,
+        searchAndReplaceContentUrls: {
+          sourceUrl: "https://chivsjawn.wordpress.com/",
+          replacementUrl: "https://localhost:8000",
+        },
+        auth: {
+          wpcom_app_clientSecret: process.env.WORDPRESS_CLIENT_SECRET,
+          wpcom_app_clientId: "54793",
+          wpcom_user: "watsontrevor10@gmail.com",
+          wpcom_pass: process.env.WORDPRESS_PASSWORD,
+        },
+        // Set how many simultaneous requests are sent at once.
+        concurrentRequests: 10,
+        includedRoutes: [
+          "**/categories",
+          "**/posts",
+          "**/pages",
+          "**/media",
+          "**/tags",
+          "**/taxonomies",
+          "**/users",
+        ],
+        excludedRoutes: [],
+        normalizer: function({ entities }) {
+          return entities
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
