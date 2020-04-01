16db412bd44fde953d9713fa39426edcca7ac60e
watsontrevor10 <watsontrevor10@gmail.com>
2020-04-01 13:35:33 -0600

    removed an item

--- src/pages/tyqcolor.js
+++ src/pages/tyqcolor.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
-import styled from "styled-components"
 
 // Images
 import tyqcolor from "../images/tyqcolor.png"
